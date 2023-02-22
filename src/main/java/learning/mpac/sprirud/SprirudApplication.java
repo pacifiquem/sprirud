a9e50b2fe3a0bc3268f7944886f8616d2f1b5b14
pacifiquem <pacifiquemurangwa001@gmail.com>
2023-02-22 08:48:58 +0200

    init: finished initializing project

---
+++ src/main/java/learning/mpac/sprirud/SprirudApplication.java
@@ -0,0 +1,13 @@
+package learning.mpac.sprirud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprirudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprirudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/learning/mpac/sprirud/SprirudApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.mpac.sprirud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprirudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
