f320450d0abbf7c85491c9e53dcc8d38a98f995d
pacifiquem <pacifiquemurangwa001@gmail.com>
2023-03-02 10:49:27 +0200

    feat: added first controller

--- src/main/java/learning/mpac/sprirud/SprirudApplication.java
+++ src/main/java/learning/mpac/sprirud/SprirudApplication.java
@@ -2,12 +2,20 @@ package learning.mpac.sprirud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SprirudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SprirudApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String welcome() {
+		return "Welcome to sprirud";
 	}
 
 }
