602a7ff57a959b1f08d9c030e781f5d0d09953db
M.Pac <pacifiquemurangwa001@gmail.com>
2023-05-19 16:28:09 +0200

    feat: re-initialization

--- src/main/java/learning/mpac/sprirud/SprirudApplication.java
+++ src/main/java/learning/mpac/sprirud/SprirudApplication.java
@@ -1,27 +1,15 @@
 package learning.mpac.sprirud;
 
+import learning.mpac.sprirud.classes.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import learning.mpac.sprirud.*;
 @SpringBootApplication
-@RestController
+
 public class SprirudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SprirudApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public String welcome() {
-		return "Welcome to sprirud";
-	}
-
-	@GetMapping("*")
-	public String noRoute() {
-		return "No such route on sprirud server";
 	}
 
 }
